16aa65dcf6c48bd51085aa29d371f333324ae523
DeniyiBams <101483132+DeniyiBams@users.noreply.github.com>
2022-04-08 00:03:41 +0000

    UNIX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- *main - Entry point
+ *main - Prints out the last part of a quote in the standard error.
  *
- *Return: return 1 if success
+ *Return: return 1 if success.
  */
 int main(void)
 {
