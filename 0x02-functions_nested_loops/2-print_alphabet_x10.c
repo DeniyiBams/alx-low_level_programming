0236ee82966e392e5bb504a498883fe5d4ceeb7c
DeniyiBams <101483132+DeniyiBams@users.noreply.github.com>
2022-04-12 22:19:11 +0000

    Printing the alphabets in lowercase 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,7 +3,6 @@
 /**
  * print_alphabet_x10 - prints 10 times the alphabet, in lowercase
  */
-
 void print_alphabet_x10(void)
 {
 	int count = 0;
@@ -15,7 +14,6 @@ void print_alphabet_x10(void)
 		{
 			_putchar(letter);
 		}
-
 		_putchar('\n');
 	}
 }
