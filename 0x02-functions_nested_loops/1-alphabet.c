e9e3b99ecb10a6533bc96eb21100a5e05db6d268
DeniyiBams <101483132+DeniyiBams@users.noreply.github.com>
2022-04-12 17:45:08 +0000

    Printing alphabets in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char alph;
+	for(alph = 'a'; alph <= 'z'; alph++)
+	{
+		_putchar(alph);
+	}
+
+	_putchar("\n");
+
+	return (0);
+}
