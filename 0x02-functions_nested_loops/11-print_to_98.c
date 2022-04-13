7d37165cc2037c1fddf2655ded6e437411d59351
DeniyiBams <101483132+DeniyiBams@users.noreply.github.com>
2022-04-13 01:05:31 +0000

    Printing of all natural numbers to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_to_98 - To print all natural numbers to 98
+ * @n: number in question.
+ */
+
+void print_to_98(int n)
+{
+	for (n = 0; n <= 98; n++)
+	{
+		_putchar(n + '0');
+		_putchar(',');
+		_putchar(' ');
+		
+		if (n < 0)
+		{
+			n++;
+		}
+		else if (n > 98)
+			n--+;
+	}
+}
