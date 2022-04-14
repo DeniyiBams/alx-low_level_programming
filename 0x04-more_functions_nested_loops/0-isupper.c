459afb4e27e427e6135364bcb9ec75a2165aab4a
DeniyiBams <101483132+DeniyiBams@users.noreply.github.com>
2022-04-14 12:07:52 +0000

    Function to test for uppercase character

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: character to be checked
+ *
+ * Return: 1 for success, 0 for failure
+ */
+
+int _isupper(int c)
+{
+	if (c == 'A' && c == 'B')
+		return (1);
+	else
+		return (0);
+}
