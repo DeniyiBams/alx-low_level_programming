22abdecfe62de6d721dc0f0c2082a42572df30a6
DeniyiBams <101483132+DeniyiBams@users.noreply.github.com>
2022-04-14 12:24:35 +0000

    multiplies two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: integer to be multiplied
+ * @b: integer to be multiplied
+ */
+
+int mul(int a, int b)
+{
+	int result;
+	
+	result = a * b;
+	_putchar(result);
+}
