abfdf6deafac8e87c5cf0a8149daf799653e3e9c
DeniyiBams <101483132+DeniyiBams@users.noreply.github.com>
2022-04-14 13:02:56 +0000

    printing numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the number
+ */
+
+void more_numbers(void)
+{
+	int num;
+	int count = 0;
+
+	while (count != 10)
+	{
+		for (num = '0'; num <= '14'; num++)
+			_putchar(num);
+		_putchar('\n');
+		
+		count++;
+	}
+}
