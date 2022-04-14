443c3d586195848c387244b4f95c88d9a6fb1733
DeniyiBams <101483132+DeniyiBams@users.noreply.github.com>
2022-04-14 20:57:46 +0000

    Printing squares using #

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square made of #
+ * @size: size of the square
+ */
+
+void print_square(int size)
+{
+	int i, j;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
