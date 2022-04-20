846323d8e066f3bb31848eaaac8d7ffd215f53fa
DeniyiBams <101483132+DeniyiBams@users.noreply.github.com>
2022-04-20 22:35:15 +0000

    Program to generate random valid passwords

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - creates a password
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int key_num, symbol, sum_check;
+
+	srand(time(NULL));
+	sum_check = 2300;
+	key_num = 0;
+
+	while (key_num < (sum_check - 122))
+	{
+		symbol = (rand() % (122 - 97 + 1)) + 97; /*to print mostly lowercase*/
+		printf("%c", symbol);
+		key_num = key_num + symbol;
+	}
+
+	symbol = sum_check - key_num;
+	printf("%c", symbol);
+
+	return(0);
+}
