cebd638c41dac1ee39a1085582352416c5a40e4b
DeniyiBams <101483132+DeniyiBams@users.noreply.github.com>
2022-04-20 21:04:17 +0000

    Function to print n times element in an array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,19 +9,13 @@
 
 void print_array(int *a, int n)
 {
-	int i = 0;
-	int j;
+	int i;
 
-	while (a[i] != '\0')
-		i++;
-
-	j = 0;
-
-	while (j < i && j < n)
+	for (i = 0; i < n; i++)
 	{
-		printf("%d, ", a[j]);
+		printf("%d", a[i]);
 
-		if (j < n - 1)
+		if (i < n - 1)
 			printf(", ");
 		j++;
 	}
