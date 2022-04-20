799803d485126724cb454c6a4cfaa25abc996f8f
DeniyiBams <101483132+DeniyiBams@users.noreply.github.com>
2022-04-20 20:25:07 +0000

    Function to print the second half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * puts_half - function to print the second half of a string
+ * @str - string to be printed
+ */
+
+void puts_half(char *str)
+{
+	int i, j, k;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	if (i % 2 == 0)
+	{
+		k = i / 2;
+
+		for (j = k; str[j] != '\0'; j++)
+		       _putchar(str[j]);
+	}
+	else
+	{
+		k = (i - 1) / 2;
+
+		for (j = k; str[j] != '\0'; j++)
+			_putchar(str[j]);
+	}
+	_putchar('\n');
+}
