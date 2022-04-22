bcbbda7d0c11dc7ec9b53d88b00b0fe947148725
DeniyiBams <101483132+DeniyiBams@users.noreply.github.com>
2022-04-22 15:57:26 +0000

    Function to concantenate two strings and specify the number of bytes

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,11 +10,13 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int i;
-	int j = 0;
+	int i, j;
 
 	for (i = 0; dest[i] != '\0'; i++)
 		;
+
+	j = 0;
+
 	while (j < n && src[j] != '\0')
 	{
 		dest[i] = src[j];
