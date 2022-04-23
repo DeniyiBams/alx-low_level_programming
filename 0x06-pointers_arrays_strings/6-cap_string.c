c0421a500c4154879c29207c34f9dd7d1c04d578
DeniyiBams <101483132+DeniyiBams@users.noreply.github.com>
2022-04-23 16:05:02 +0000

    Function to capitalize words in a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * cap_string - function to capitalize all words of a string.
+ * @dest: string
+ */
+
+char *cap_string(char *dest)
+{
+	int i, j;
+	int a[] = {32, 125, 123, 41, 40, 34, 63, 33, 46, 59, 44, 10, 9};
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		if (dest[i] >= 'a' && dest[i] <= 'z')
+			if (i == 0)
+				dest[i] = dest[i] = 32;
+			else
+				for (j = 0; j < 13; j++)
+				{
+					if (a[j] == dest[i - 1])
+						dest[i] = dest[i] - 32;
+				}
+	}
+}
