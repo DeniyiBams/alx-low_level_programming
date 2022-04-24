b2bc238b715695df69e8074fb3dc84761f4c9c68
DeniyiBams <101483132+DeniyiBams@users.noreply.github.com>
2022-04-24 11:25:26 +0000

    Function to print integers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,16 +7,41 @@
 
 void print_number(int n)
 {
-        unsigned int i = n;
+	int i, j, digit, digits, power;
+	unsigned int temp, numchar, number;
 
-        if (n < 0)
-        {
-                _putchar(45);
-                i = -i;
-        }
-        if (i / 10)
-        {
-                print_number(i / 10);
-        }
-        _putchar(i % 10 + '0');
+	digit = 0;
+	if (n < 0)
+	{
+		_putchar('-');
+		temp = -n;
+	}
+	else
+	{
+		temp = n;
+	}
+
+	number = temp;
+
+	while (number >= 10)
+	{
+		number = number / 10;
+		digit++;
+	}
+	digits = digit + 1;
+	power = 1;
+	i = 1;
+
+	while (i < digits)
+	{
+		power = power * 10;
+		i++;
+	}
+	j = power;
+	while (j >= 1)
+	{
+		numchar = (temp / j) % 10;
+		_putchar(numchar + '0');
+		j = j / 10;
+	}
 }
