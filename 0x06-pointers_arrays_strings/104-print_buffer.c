31224f44df672aba5f0af1c2c77ad76b65a2471c
DeniyiBams <101483132+DeniyiBams@users.noreply.github.com>
2022-04-24 16:49:23 +0000

    Function to print a buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -2,43 +2,43 @@
 #include <stdio.h>
 
 /**
- * print_line - print a s bytes of a buffer
+ * print_line - prints a s bytes of a buffer
  * @c: buffer to print
- * @s: bytes of buffer
- * @l: line of buffer
+ * @s: bytes of buffer to print
+ * @l: line of buffer to print
+ *
+ * Return: void
  */
 
 void print_line(char *c, int s, int l)
 {
 	int j, k;
 
-	j = 0;
-
-	while (j < 10)
+	for (j = 0; j <= 9; j++)
 	{
 		if (j <= s)
 			printf("%02x", c[l * 10 + j]);
 		else
-			printf(" ");
+			printf("  ");
 		if (j % 2)
-			printf(" ");
-		j++;
+			putchar(' ');
 	}
 	for (k = 0; k <= s; k++)
 	{
 		if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
 			putchar(c[l * 10 + k]);
 		else
-			printf(".");
+			putchar('.');
 	}
 }
 
 /**
  * print_buffer - prints a buffer
- * @b: buffer
+ * @b: buffer to print
  * @size: size of buffer
+ *
+ * Return: void
  */
-
 void print_buffer(char *b, int size)
 {
 	int i;
@@ -47,11 +47,15 @@ void print_buffer(char *b, int size)
 	{
 		printf("%08x: ", i * 10);
 		if (i < size / 10)
+		{
 			print_line(b, 9, i);
+		}
 		else
+		{
 			print_line(b, size % 10 - 1, i);
-		printf("\n");
+		}
+		putchar('\n');
 	}
 	if (size == 0)
-		printf("\n");
+		putchar('\n');
 }
