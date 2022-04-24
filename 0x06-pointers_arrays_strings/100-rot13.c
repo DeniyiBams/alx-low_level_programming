2cc18c8cea1362b1defdbf1897ffdb1074c7681d
DeniyiBams <101483132+DeniyiBams@users.noreply.github.com>
2022-04-24 11:08:16 +0000

    Change letters to Rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+include "main.h"
+
+/**
+ * rot13 - change letters to ROT13
+ * @s: string to be analized
+ *
+ * Return: string in question
+ */
+
+char *rot13(char *s)
+{
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = rot[j];
+				break;
+			}
+		}
+	}
+
+	return (s);
+}
