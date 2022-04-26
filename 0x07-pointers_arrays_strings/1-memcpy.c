18aec1a90361521a26ddddfb08a38aaee2150c70
DeniyiBams <101483132+DeniyiBams@users.noreply.github.com>
2022-04-26 15:04:18 +0000

    Function

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,15 +10,11 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i, j;
+	unsigned int i;
 
-	j = 0
-
-	for (i = 0; src[i] != '\0'; i++)
+	for (i = 0; i < n; i++)
 	{
-		dest[j] = src[i];
-		j++;
+		dest[i] = src[i];
 	}
-
 	return (dest);
 }
