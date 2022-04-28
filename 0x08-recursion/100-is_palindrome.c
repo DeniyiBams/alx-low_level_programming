afe0f3b6e811feab3a15c234aa36c346db613a77
DeniyiBams <101483132+DeniyiBams@users.noreply.github.com>
2022-04-28 16:14:59 +0000

    Function to check for palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * is_palindrome - function to check for palindrome
+ * @s: string
+ *
+ * Return: 1 if palindrome, else 0
+ */
+
+int is_palindrome(char *s)
+{
+	if (*s == '\0')
+		return (1);
+	else
+		return (0);
+}
