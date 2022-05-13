9031e11d3899968e77e9c17ebcda7fe834a9d4bf
DeniyiBams <101483132+DeniyiBams@users.noreply.github.com>
2022-05-13 17:35:10 +0000

    Function to print a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - function to print a name
+ * @name: name to be printed
+ * @f: function to print
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL && f == NULL)
+		return;
+	else
+		f(name);
+}
