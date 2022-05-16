7bfdc282bdf2c136a096ebb3187651c87016b57f
DeniyiBams <101483132+DeniyiBams@users.noreply.github.com>
2022-05-16 14:24:01 +0000

    Function

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -17,6 +17,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list args;
 
 	va_start(args, n);
+
 	for (i = 0; i < n; i++)
 	{
 		num = va_arg(args, int);
@@ -25,6 +26,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		if (i < n - 1 && separator)
 			printf("%s", separator);
 	}
-	printf("\n");
 	va_end(args);
+
+	printf("\n");
 }
