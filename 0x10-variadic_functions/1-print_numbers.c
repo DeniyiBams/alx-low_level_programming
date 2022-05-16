0ce6a211d7ec44f7994a0c86ed5688b9afa70ecc
DeniyiBams <101483132+DeniyiBams@users.noreply.github.com>
2022-05-16 14:04:53 +0000

    To print numbers with a seperator

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,30 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - to print numbers seperated by a seperator
+ * @seperator: string to be printed between numbers
+ * @n: number of integers passed
+ */
+
+void print_numbers(const char *seperator, const unsigned int n, ...)
+{
+        unsigned int i, x;
+        va_list args;
+
+        va_start(args, n);
+
+        if (seperator != NULL)
+        {
+                for (i = 0; i < n; i++)
+                {
+                        x = va_arg(args, int);
+                        printf("%d", x);
+                        if (i < n - 1)
+                            printf("%s", seperator);
+                }
+        }
+        printf("\n");
+	va_end(args);
+}
