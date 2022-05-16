dc6851756a31324639ee306a540a3eb93bd4a2ab
DeniyiBams <101483132+DeniyiBams@users.noreply.github.com>
2022-05-16 14:35:24 +0000

    To print strings followed by a separator

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadics_functions.h"
+
+/**
+ * print_strings - function to print strings
+ * @separator: seperator
+ * @n: number of strings passed
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	char *str;
+	va_list args;
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(args, char *);
+		if (str)
+			printf("%s", c);
+		else
+			printf("(nil)");
+		if (i < n - 1 && separator)
+			printf("%s", separator);
+	}
+	va_end(args);
+	printf("\n");
+}
