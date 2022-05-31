f1a2f64e50815071e269cbf975862efcc22fad57
DeniyiBams <101483132+DeniyiBams@users.noreply.github.com>
2022-05-31 00:28:00 +0000

    Error fix

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,23 +9,8 @@
 
 void print_binary(unsigned long int n)
 {
-	int i, j;
-	unsigned long int value, extract = 0;
+	if (n > 1)
+		print_binary(n >> 1);
 
-	if (n == 0)
-		_putchar('0');
-
-	value = n;
-
-	for (j = 0; value != 0; j++)
-		value = value >> 1;
-	for (i = j - 1; i >= 0; i--)
-	{
-		extract = 1 << i;
-
-		if ((n & extract) != 0)
-			_putchar('1');
-		else
-			_putchar('0');
-	}
+	_putchar((n & 1) + 48);
 }
